2c323275135b0a0e9f21e598e4bf082a3b2993af
HYSA <aj.anunciado@gmail.com>
2023-09-05 15:12:38 +0800

    1

---
+++ Second Activity/calculator.js
@@ -0,0 +1,33 @@
+let submit = document.getElementById("submit");
+
+const loan = () => {
+  let lamount = parseFloat(document.getElementById("loanAmount").value);
+  let months = parseFloat(document.getElementById("month").value);
+  let computeAmount=document.getElementById("computeAmount")
+  let amonths = new Number(document.getElementById("amountmonth"));
+
+  if (lamount >= 10000) {
+    lamount = lamount + lamount * 0.035;
+//   } else if (lamount < 10000) {
+   
+//     return lamount;
+//   }
+  }
+  if (months >= 18) {
+    lamount = lamount + lamount * 0.03;
+  } else if (months >= 12 || months <18) {
+    lamount = lamount + lamount * 0.02;
+  }
+
+  console.log(lamount);
+
+ amonths = lamount / months;
+  console.log(lamount.toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+  console.log(amonths.toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
+  document.getElementById("computeAmount").value = lamount.toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+  document.getElementById("amountMonth").value = amonths.toLocaleString('en', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+};
+
+submit.addEventListener('click', function () {
+  loan();
+});
